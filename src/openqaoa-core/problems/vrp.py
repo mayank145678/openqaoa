6d4e85a93d84816000b536bcfbad97fb6655e415
mayank145678 <133011154+mayank145678@users.noreply.github.com>
2023-05-09 21:54:52 +0530

    Update vrp.py

--- src/openqaoa-core/problems/vrp.py
+++ src/openqaoa-core/problems/vrp.py
@@ -214,7 +214,7 @@ class VRPMatrix(Problem):
         # Minimize distance traveled
         mdl.minimize(
             mdl.sum(
-                self.distance_matrix[i][j] * x[(i, j)]
+                self.distance_matrix[i][j] * x[(i, j)]/100
                 for i in range(num_nodes - 1)
                 for j in range(i + 1, num_nodes)
             )
